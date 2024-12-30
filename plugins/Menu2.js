50e5478ed089dce295def240621aa13687bf6cd5
Md Jahid Hasan Rajib <lovefack79@gmail.com>
2024-12-30 13:17:33 +0600

    Update Menu2.js

--- plugins/Menu2.js
+++ plugins/Menu2.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
     let _uptime = process.uptime() * 1000
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+if (!(who in global.db.data.users)) throw `The user is not found in my database...🛰️`
 let pp = './assets/A.jpg'
 let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
